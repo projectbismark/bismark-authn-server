f51588121f6107fa7ae6577b7799070a14a026fc
Andrew Kahn <xakahnx@gmail.com>
2012-07-18 18:05:47 -0400

    admin style

--- oauth2/admin.py
+++ oauth2/admin.py
@@ -7,15 +7,19 @@ admin.site.unregister(User)
 
 class RouterInline(admin.StackedInline): 
     model = Router
+    extra = 0
 
 class ClientInline(admin.StackedInline): 
     model = Client
+    extra = 0
 
 class CodeInline(admin.StackedInline): 
     model = Code
+    extra = 0
 
 class AccessTokenInline(admin.StackedInline): 
     model = AccessToken
+    extra = 0
 
 class UserAdmin(admin.ModelAdmin): 
     fieldsets = [
