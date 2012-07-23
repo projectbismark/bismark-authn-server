fc173b07d831dc87ee2969eadbc1db0260730602
Andrew Kahn <xakahnx@gmail.com>
2012-07-23 12:52:31 -0400

    small fix

--- bismark-app/forms.py
+++ bismark-app/forms.py
@@ -7,7 +7,7 @@ class AuthorizeForm(forms.Form):
 
 class InfoForm(forms.Form):
     isp = forms.CharField(label="ISP", max_length=75, widget=forms.TextInput(attrs=dict(maxlength=75)))
-    service_type = forms.ChoiceField(label="Service Type", choices=(('1', 'DSL'), ('2', 'Fiber'), ('3', 'Cable'), ('4', 'Wireless- Cellular'), ('5', 'Wireless- WiMAX'), ('6', 'Wireless- 802.11'), ('7', 'Other')))
+    service_type = forms.ChoiceField(label="Service Type", choices=(('DSL', 'DSL'), ('Fiber', 'Fiber'), ('Cable', 'Cable'), ('Wireless-Cellular', 'Wireless- Cellular'), ('Wireless- WiMAX', 'Wireless- WiMAX'), ('Wireless-802.11', 'Wireless- 802.11'), ('Other', 'Other')))
     service_plan = forms.CharField(label="Service Plan", max_length=75, widget=forms.TextInput(attrs=dict(maxlength=75)))
     drate = forms.IntegerField(label="Download Rate")
     urate = forms.IntegerField(label="Upload Rate")
