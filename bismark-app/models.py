c1d7c74288b212d23b12b9145c6bcad2e7712b03
Sam Burnett <srburnet@gmail.com>
2013-08-14 01:45:01 -0400

    Formatting

--- bismark-app/models.py
+++ bismark-app/models.py
@@ -4,15 +4,15 @@ from django_countries import CountryField
 
 #Router object stores user information
 class Router(models.Model):
-	def __unicode__(self): 
-		 return self.user.email
-	user = models.ForeignKey(User)
-	isp = models.CharField(max_length=100)
-	city = models.CharField(max_length=100)
-	state = models.CharField(max_length=100)
-	country = CountryField()
-	service_type = models.CharField(max_length=100)
-	service_plan = models.CharField(max_length=100)
-	drate = models.CharField(max_length=100, blank=True)
-	urate = models.CharField(max_length=100, blank=True)
-	node_id = models.CharField(max_length=100)
+    def __unicode__(self): 
+         return self.user.email
+    user = models.ForeignKey(User)
+    isp = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = CountryField()
+    service_type = models.CharField(max_length=100)
+    service_plan = models.CharField(max_length=100)
+    drate = models.CharField(max_length=100, blank=True)
+    urate = models.CharField(max_length=100, blank=True)
+    node_id = models.CharField(max_length=100)
--- bismark-app/views.py
+++ bismark-app/views.py
@@ -15,54 +15,54 @@ from django.contrib.auth.forms import UserCreationForm
 import time
 
 @login_required
-def router(request): 
-    """Page attempts to redirect and pass token to router url. 
-	On GET emulates myrouter.projectbismark.net. On failure, user enters gateway into form which is handled by POST."""
+def router(request):
+    """Page attempts to redirect and pass token to router url.
+        On GET emulates myrouter.projectbismark.net. On failure, user enters gateway into form which is handled by POST."""
 
     client = Client.objects.get(user=request.user)
     token = AccessToken.objects.filter(client=client).select_related().order_by("expire").reverse()
-    if request.method == 'POST': 
-	return redirect('http://' + request.POST.get('gateway') + '/cgi-bin/luci/oauth/genkey?token=' + token[0].token)
+    if request.method == 'POST':
+        return redirect('http://' + request.POST.get('gateway') + '/cgi-bin/luci/oauth/genkey?token=' + token[0].token)
     template ={
-	'token': token[0].token}
+        'token': token[0].token}
     return render_to_response(
         'oauth2/router.html',
         template,
         RequestContext(request))
 
 @login_required
-def profile(request): 
+def profile(request):
     """Generic profile page, contains navigation to edit profile and download
     token.  """
     template = {
-	'user': request.user.email, 
-	'router': Router.objects.filter(user=request.user)}
+        'user': request.user.email,
+        'router': Router.objects.filter(user=request.user)}
     return render_to_response(
-	'registration/profile.html', 
-	template, 
-	RequestContext(request))
+        'registration/profile.html',
+        template,
+        RequestContext(request))
 
 @login_required
-def cont_register(request): 
+def cont_register(request):
     """Page contains router info form and creates user's router object. On submit begins OAuth protocol. """
     if request.method == 'POST':
-	info_form = InfoForm(data=request.POST)
+        info_form = InfoForm(data=request.POST)
         if info_form.is_valid():
-	    user = request.user
-	    if not Router.objects.filter(user=user).count() > 0: 
-	        router = Router.objects.create(user=user)
-	    else: 
-	        router = Router.objects.get(user=user)
-	    vars = ('isp', 'service_type', 'service_plan', 'drate', 'urate', 'city', 'state', 'country', 'node_id')
-	    for i in vars: 
-	        router.__setattr__(i, request.REQUEST.get(i))
-	    router.save()
-	    return HttpResponseRedirect("/oauth2/authorize?redirect_uri=https%3A%2F%2Fregister.projectbismark.net%2Fclient%2F&response_type=code")
+            user = request.user
+            if not Router.objects.filter(user=user).count() > 0:
+                router = Router.objects.create(user=user)
+            else:
+                router = Router.objects.get(user=user)
+            vars = ('isp', 'service_type', 'service_plan', 'drate', 'urate', 'city', 'state', 'country', 'node_id')
+            for i in vars:
+                router.__setattr__(i, request.REQUEST.get(i))
+            router.save()
+            return HttpResponseRedirect("/oauth2/authorize?redirect_uri=https%3A%2F%2Fregister.projectbismark.net%2Fclient%2F&response_type=code")
     else:
-	if not Router.objects.filter(user=request.user).count() > 0:
+        if not Router.objects.filter(user=request.user).count() > 0:
             info_form = InfoForm()
-	else: 
-	    info_form = InfoForm(data=Router.objects.filter(user=request.user).values()[0])
+        else:
+            info_form = InfoForm(data=Router.objects.filter(user=request.user).values()[0])
     context = { 'info_form':info_form, 'user':request.user }
     return render_to_response('registration/cont_register.html', context, context_instance=RequestContext(request))
 
@@ -71,8 +71,8 @@ def cont_register(request):
 def missing_redirect_uri(request):
     """Error page required by oauth2app. """
     return render_to_response(
-        'oauth2/missing_redirect_uri.html', 
-        {}, 
+        'oauth2/missing_redirect_uri.html',
+        {},
         RequestContext(request))
 
 
@@ -85,7 +85,7 @@ def authorize(request):
     except MissingRedirectURI, e:
         return HttpResponseRedirect("/oauth2/missing_redirect_uri")
     except AuthorizationException, e:
-        # The request is malformed or invalid. Automatically 
+        # The request is malformed or invalid. Automatically
         # redirects to the provided redirect URL.
         return authorizer.error_redirect()
     return authorizer.grant_redirect()
@@ -104,7 +104,7 @@ def client(request):
     if AccessToken.objects.filter(client=client).select_related().count() > 0 and token[0].expire > time.time():
         return HttpResponseRedirect('/router')
     else:
-	return render_to_response(
+        return render_to_response(
             'oauth2/client.html',
             template,
             RequestContext(request))
